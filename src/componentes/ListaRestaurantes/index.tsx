e53bddb43857e6bf09869097a277b5fdac7062de
joaovitormelo7Note <joaovitormelo199@gmail.com>
2024-09-18 00:46:44 -0300

    adicionando setor de próxima página e página anterior

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -7,44 +7,44 @@ import { IPaginacao } from '../../interfaces/IPaginacao';
 
 const ListaRestaurantes = () => {
 
-  
   const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
   const [proximaPagina, setProximaPagina] = useState('')
+  const [paginaAnterior, setPaginaAnterior] = useState('')
 
-  useEffect(() => {
-    axios.get<IPaginacao<IRestaurante>>('http://localhost:8000/api/v1/restaurantes/')
+  const carregarDados = (url: string) => {
+    if(!url) return;
+
+    axios.get<IPaginacao<IRestaurante>>(url)
     .then(resposta => {
+      console.log('resposta do api:', resposta.data)
       setRestaurantes(resposta.data.results)
       setProximaPagina(resposta.data.next)
-    })
-    .catch(erro => {
-      console.log(erro)
-    })
-  }, [])
-
-  const verMais = () => {
-    axios.get<IPaginacao<IRestaurante>>(proximaPagina)
-    .then(resposta => {
-      setRestaurantes([...restaurantes, ...resposta.data.results])
-      setProximaPagina(resposta.data.next)
+      setPaginaAnterior(resposta.data.previous)
     })
     .catch(erro => {
       console.log(erro)
     })
   }
+  
+
+  useEffect(() => {
+    carregarDados('http://localhost:8000/api/v2/restaurantes/')
+  }, [])
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bem avaliados do país</em>!</h1>
     {restaurantes?.map(item => <Restaurante restaurante={item} key={item.id} />)}
-    {proximaPagina && <button onClick={verMais}>
-        Ver mais
-      </button>}
+    {<button 
+        onClick={() => carregarDados(paginaAnterior)} 
+        disabled={!paginaAnterior}>
+            Página anterior
+    </button>}
+    {<button 
+        onClick={() => carregarDados(proximaPagina)} 
+        disabled={!proximaPagina}>
+            Próxima página
+    </button>}
   </section>)
-
-
 }
-
-
-
 
 export default ListaRestaurantes
