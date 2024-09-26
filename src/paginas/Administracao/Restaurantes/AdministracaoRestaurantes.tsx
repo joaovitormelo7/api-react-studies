a51a3d4216ccc1220657e2f292288e57641b8b14
João Vitor Melo <joaovitormelo199@gmail.com>
2024-09-25 21:59:17 -0300

    editando restaurantes

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import IRestaurante from "../../../interfaces/IRestaurante"
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material"
 import axios from "axios"
+import { Link } from "react-router-dom"
 
 const AdministracaoRestaurantes = () => {
 
@@ -26,6 +27,9 @@ const AdministracaoRestaurantes = () => {
                     {restaurantes.map(restaurante =><TableRow key={restaurante.id}>
                         <TableCell>
                             {restaurante.nome}
+                        </TableCell>
+                        <TableCell>
+                            <Link to={`/admin/restaurantes/${restaurante.id}`}>editar</Link>
                         </TableCell>
                     </TableRow> 
                     )}
