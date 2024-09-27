268cfcb3220c5d9687faeb1c95453fae91ef571d
João Vitor Melo <joaovitormelo199@gmail.com>
2024-09-27 00:45:23 -0300

    refatorando o axios

--- src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurantes.tsx
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react"
 import IRestaurante from "../../../interfaces/IRestaurante"
 import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Button } from "@mui/material"
-import axios from "axios"
 import { Link } from "react-router-dom"
-import ListaRestaurantes from "../../../componentes/ListaRestaurantes"
+import http from "../../../http"
+
 
 const AdministracaoRestaurantes = () => {
 
     const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([])
 
     useEffect(() =>{
-        axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+        http.get<IRestaurante[]>('restaurantes/')
             .then(resposta => setRestaurantes(resposta.data))
     }, [])
     
     const excluir = (restauranteExclusao: IRestaurante) => {
-        axios.delete(`http://localhost:8000/api/v2/restaurantes/${restauranteExclusao.id}/`)
+        http.delete(`restaurantes/${restauranteExclusao.id}/`)
             .then(() => {
                 const restauranteFilter = restaurantes.filter(restaurante => restaurante.id !== restauranteExclusao.id)
                 setRestaurantes([ ...restauranteFilter ])
--- src/paginas/Administracao/Restaurantes/FormsRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormsRestaurante.tsx
@@ -1,17 +1,17 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
-import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import IRestaurante from "../../../interfaces/IRestaurante";
+import http from "../../../http";
 
 const FormularioRestaurante = () => {
   const parametros = useParams();
 
   useEffect(() => {
     if (parametros.id) {
-      axios
+      http
         .get<IRestaurante>(
-          `http://localhost:8000/api/v2/restaurantes/${parametros.id}/`
+          `restaurantes/${parametros.id}/`
         )
         .then((resposta) => setNomeRestaurante(resposta.data.nome));
     }
@@ -23,16 +23,16 @@ const FormularioRestaurante = () => {
     evento.preventDefault();
 
     if (parametros.id) {
-      axios
-        .put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+      http
+        .put(`restaurantes/${parametros.id}/`, {
           nome: nomeRestaurante,
         })
         .then(() => {
           alert("Restaurante atualizado com sucesso!");
         });
     } else {
-      axios
-        .post("http://localhost:8000/api/v2/restaurantes/", {
+      http
+        .post("restaurantes/", {
           nome: nomeRestaurante,
         })
         .then(() => {
